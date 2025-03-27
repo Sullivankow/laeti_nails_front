5970a0c4ffc4b1cad173e26b43f88f28f6c668b7
SunDev <sundly@live.fr>
2025-03-27 15:45:58 +0100

    Config saas OK

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,34 @@
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.scss';
 import './App.css'
-
 import Navbar from "./components/Navbar"
+
 
 function App() {
 
 
   return (
+  
     <div>
 
-<div>
-<Navbar />
-        
 
+      <div>
+      <Navbar />
 </div>
 
 
-
-
-      
 
 
 
 
 
     </div>
+
+
+
+
+
   )
 }
 
