a6de05b755ce14d691fb60099c8ca0faab8e9ae2
SunDev <sundly@live.fr>
2025-03-27 15:53:19 +0100

    Navbar OK

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,37 +1,55 @@
+import { useState } from "react";
+
 function Navbar() {
-    return (
-      <nav className="navbar navbar-expand-lg navbar-light bg-primary">
-        <div className="container-fluid">
-          <a className="navbar-brand text-white" href="#">Laeti Nails</a>
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav">
-              <li className="nav-item">
-                <a className="nav-link active text-white" aria-current="page" href="#">Home</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link text-white" href="#">Features</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link text-white" href="#">Pricing</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link disabled text-white" aria-disabled="true">Disabled</a>
-              </li>
-            </ul>
-          </div>
-          <div>
-            <button className="btn btn-secondary">Bouton personnalisé</button>
-            <p className="text-danger">Texte en rouge</p>
-          </div>
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-primary">
+      <div className="container-fluid d-flex justify-content-between align-items-center">
+        {/* Logo */}
+        <a className="navbar-brand text-white" href="#">Laeti Nails</a>
+
+        {/* Bouton Burger */}
+        <button 
+          className="navbar-toggler" 
+          type="button" 
+          onClick={() => setIsOpen(!isOpen)}
+          aria-controls="navbarNav" 
+          aria-expanded={isOpen} 
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        {/* Menu centré */}
+        <div className={`collapse navbar-collapse justify-content-center ${isOpen ? "show" : ""}`} id="navbarNav">
+          <ul className="navbar-nav text-center">
+            <li className="nav-item">
+              <a className="nav-link active text-white" aria-current="page" href="#">Home</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link text-white" href="#">Features</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link text-white" href="#">Pricing</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link disabled text-white" aria-disabled="true">Disabled</a>
+            </li>
+          </ul>
         </div>
-      </nav>
-    );
-  }
-  
-  export default Navbar;
+
+        {/* Bouton Connexion */}
+        <div>
+          <button className="btn btn-secondary">Connexion</button>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
+
   
 
 
